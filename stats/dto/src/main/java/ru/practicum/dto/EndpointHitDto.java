47945f13819c43aeed2b7198e89a00ae05886863
Mihey <miheyzhukov@yandex.ru>
2025-06-27 02:22:30 +0300

    Исправление ошибок 2

--- stats/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,5 +17,6 @@ public class EndpointHitDto {
     private String app;
     private String uri;
     private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
